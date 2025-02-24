32a2105689553f1c246b4b2a221d59050e8a94bd
projectOAT <project.oat007@gmail.com>
2025-02-24 20:24:55 +0530

    Delete query.py

--- query.py
+++
@@ -1,9 +0,0 @@
-# query.py
-
-from agent import agent_executor
-
-# Ask a sample question
-question = "What is the highest mark in Mathematics?"
-response = agent_executor.invoke(question)
-
-print("Answer:", response)
