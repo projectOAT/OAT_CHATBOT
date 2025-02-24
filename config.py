6c75809ee61f9b6687c332eb27c357140d1e92a6
projectOAT <project.oat007@gmail.com>
2025-02-24 20:24:31 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_HOST = "localhost"
-DB_NAME = "co_attainment"
-
-LLAMA_MODEL_PATH = "H:/llama_sql_agent/models/Meta-Llama-3-MoE-4x8B-Instruct.Q4_K_M.gguf"
